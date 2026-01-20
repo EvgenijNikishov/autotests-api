3674dae81f72d3e321cebcb3a67863357ecceb62
EvgenijNikishov <evgenij.nikishov95@gmail.com>
2026-01-20 08:48:09 +0300

    add schema with users, files

--- api_client_get_user.py
+++ api_client_get_user.py
@@ -1,27 +1,30 @@
-from clients.private_http_builder import AuthenticationUserDict
+from clients.private_http_builder import AuthenticationUserSchema
 from clients.user.private_users_client import get_private_users_client
-from clients.user.public_users_client import get_public_users_client, UserCreateRequestDict
+from clients.user.public_users_client import get_public_users_client
+# Вместо CreateUserRequestDict импортируем CreateUserRequestSchema
+from clients.user.users_schema import CreateUserRequestSchema
 from faker import get_random_email
 
 public_users_client = get_public_users_client()
 
-create_user_request = UserCreateRequestDict (
+# Вместо CreateUserRequestDict используем CreateUserRequestSchema
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
+    last_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
+    first_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
+    middle_name="string"  # Передаем аргументы в формате snake_case вместо camelCase
 )
+create_user_response = public_users_client.create_user(create_user_request)
+print('Create user data:', create_user_response)
 
-client_user_response = public_users_client.create_user_api(create_user_request)
-client_user_response_data = client_user_response.json()
-
-authentication_user=AuthenticationUserDict (
-    email=create_user_request["email"],
-    password="string"
+# Используем атрибуты вместо ключей
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
 )
-
 private_users_client = get_private_users_client(authentication_user)
-client_user_response_data_get = private_users_client.get_user_me_api()
-print(client_user_response_data_get.json())
 
+# Используем атрибуты вместо ключей
+get_user_response = private_users_client.get_user(create_user_response.user.id)
+print('Get user data:', get_user_response)
