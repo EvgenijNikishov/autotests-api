b2910d9c09e4cdf3b172e1e777d7a60699fd378f
EvgenijNikishov <evgenij.nikishov95@gmail.com>
2026-01-20 23:36:55 +0300

    add assert

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,10 +1,10 @@
-def test_user_login():
-    assert 1 == 1
+def test_first_try():  # Этот тест мы добавили в предыдущем шаге
+    print("Hello World!")
 
 
-class TestUserAuthentication:
-    def test_login(self):
-        assert 1 == 1
+def test_assert_positive_case():  # Новый тест, которые проверяет положительный кейс
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
 
-    def test_auth(self):
-        assert 1 == 1
+
+def test_assert_negative_case():  # Новый тест, которые проверяет негативный кейс
+    assert (2 + 2) == 5  # Тут должна быть ошибка
