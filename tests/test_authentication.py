ecab977c621aa1b3e169f98b8a3e4d018b1b6f9d
EvgenijNikishov <evgenij.nikishov95@gmail.com>
2026-01-21 17:06:08 +0300

    add marker for auth tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-
+import pytest
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.user.public_users_client import get_public_users_client
@@ -8,6 +8,9 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
+
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     public_users_client = get_public_users_client()
     authentication_client = get_authentication_client()
