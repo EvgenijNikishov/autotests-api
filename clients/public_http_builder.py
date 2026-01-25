3483ee82b702805326485388f62266d10cd39584
EvgenijNikishov <evgenij.nikishov95@gmail.com>
2026-01-25 23:38:49 +0300

    add curl for allure

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,10 +1,15 @@
 from httpx import Client
 
+from clients.event_hooks import curl_event_hook
+
 
 def get_public_http_client()->Client:
     """
     Функция создаёт экземпляр httpx.Client с базовыми настройками.
     :return: Готовый к использованию объект httpx.Client.
     """
-    return Client(base_url="http://localhost:8000", timeout=100)
+    return Client(base_url="http://localhost:8000",
+                  timeout=100,
+                  event_hooks={"request": [curl_event_hook]}  # Добавляем event hook для запроса
+                  )
 
